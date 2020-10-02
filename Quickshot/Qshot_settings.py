aa7e5a137788ff4e8e916c5b0996a6e210698103
can kurt <can.kurt.aa@gmail.com>
2020-10-02 04:01:14 +0300

    saving settings now dinamically changes settings on the main frame no restart required

--- Quickshot/Qshot_settings.py
+++ Quickshot/Qshot_settings.py
@@ -16,7 +16,7 @@ class Qshot_settings(QMainWindow):
     oppcity_emitter = pyqtSignal(float)
     background_color_emitter = pyqtSignal(str)
     accent_color_emitter = pyqtSignal(str)
-
+    update_fame_emitter = pyqtSignal()
 
     def __init__(self, cfg_path, icon_path):
         super().__init__()
@@ -39,6 +39,13 @@ class Qshot_settings(QMainWindow):
         cfg_status = self.read_cfg_file()
         if(cfg_status):
             self.variable_to_ui()
+    
+    def update_settings_ui(self):
+        """refreshes the settings frame without recreating ui elements"""
+        cfg_status = self.read_cfg_file()
+        if(cfg_status):
+            self.variable_to_ui()
+        self.show()
 
     def init_ui(self):
         """inits ui"""
@@ -58,20 +65,118 @@ class Qshot_settings(QMainWindow):
     def init_variables(self):
         """inits class variables"""
         self.ss_extensions_list = [".png", ".jpg"]
-        self.png_compression_level_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "-1"]
+        self.png_compression_level_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-        self.hotkey_tooltip = """
-        use <> on metakeys and + for more keys
-        ex: a+s or <ctrl>+<space>
+        self.tooltips = {
+        "colors_and_opacity" :
         """
+        Color and opacity settings.
+        """,
+
+        "hotkeys" : 
+        """
+        Specify hotkeys.
+        Use <> on metakeys and + for adding more keys.
+
+        ex: a+b+c or <ctrl>+<space>
+        """,
+
+        "save_path" : 
+        """
+        Save path for the screenshots.
+
+        'HOME' tries to find user desktop or home automatically.
+        """,
+
+        "root_file" : 
+        """
+        Root filename for screenshots.
+        Specify a filename for screenshots to be saved, 
+        it will be created under the save path you specified.
+        Leave it empty if you don't want.
+        """,
+
+        "text_before_ss-text_after_ss" : 
+        """
+        Text before and after the screenshot name.
+        Specify a text for placing before and after screenshot name.
+        Leave it empty if you don't want.
+
+        ex: 
+        (20_01_01).png  
+        screen_shot_20_01_01_end_of_ss.png
+        """,
         
+        "text_before_ss_number-text_after_ss_number" : 
+        """
+        Text before and after screenshot number.
+        If multiple screenshots exists with the same name new one will be numbered, 
+        you can specify what text will be before and after that number.
+        Leave it empty if you don't want.
+
+        ex: 
+        qs_20_01_01(1).png  
+        qs_20_01_01[1].png  
+        qs_20_01_01-1.png         
+        """,
+
+        "date_formatting" : 
+        """
+        Date formatting keywords
+
+        %Y year full
+        %y year 2 digit
+        
+        %m month digit
+        %B month text
+
+        %d day
+        %A weekday long
+        %a weekday short
+
+        %H hour 24
+        %I hour 12
+        %p am/pm
+        %M minute
+        %S second
+
+        ex: 
+        %Y-%m-%d_%H-%M 2020-01-01_01-00.png
+        %y-%B-%d-%A 20-January-01-Mon.png
+        """,
+
+        "Use_local_date_naming" : 
+        """
+        Use local date naming.
+        Check if you want date names to be your systems language.
+        """,
+
+        "extension" : 
+        """
+        Extension of the screenshots.
+        """,
+
+        "png_compression_level" :
+        """
+        Png compresion level.
+        Higher the level is smaller the png images will be.
+        """,
+
+        "multi_screen_full_ss" :
+        """
+        Multi screen full screenshot.
+        If checked, full screen screenshots will include all screens, 
+        if not only themain screen will be included.
+        """
+        }
+
         self.about_text = """
         A simple, quick, customizable screenshot tool.
         
-        <br/>
+        <br/><br/>
         <a href='https://github.com/cccaaannn/Quickshot'>Quickshot github</a>
         """
-
+        
         self.default_cfg = {
                             "general":{
                                 "background_color": "#000000",
@@ -89,10 +194,10 @@ class Qshot_settings(QMainWindow):
                                 "after_ss_name" : "", 
                                 "before_number" : "(", 
                                 "after_number" : ")", 
-                                "date_formatting" : "%y-%m-%d_%H-%M",
-                                "use_system_local_date_naming" : "1",
-                                "png_compression_level" : -1, 
-                                "multi_screen" : ""
+                                "date_formatting" : "%y-%B-%d_%H-%M",
+                                "use_system_local_date_naming" : 1,
+                                "png_compression_level" : 9, 
+                                "multi_screen" : 0
                             }
                         }
 
@@ -114,9 +219,9 @@ class Qshot_settings(QMainWindow):
                                 "before_number" : "", 
                                 "after_number" : "", 
                                 "date_formatting" : "",
-                                "use_system_local_date_naming" : "",
-                                "png_compression_level" : "", 
-                                "multi_screen" : ""
+                                "use_system_local_date_naming" : 0,
+                                "png_compression_level" : 9, 
+                                "multi_screen" : 0
                             }
                         }
 
@@ -155,7 +260,7 @@ class Qshot_settings(QMainWindow):
             return True
         except Exception as e:
             print(e)
-            self.show_alert_popup("There is a problem with cfg file please reset settings")
+            self.show_alert_popup("There is a problem with cfg file please reset settings and restart Quickshot")
             return False
     
     def variable_to_ui(self):
@@ -200,7 +305,7 @@ class Qshot_settings(QMainWindow):
 
         except Exception as e:
             print(e)
-            self.show_alert_popup("There is a problem with cfg file please reset settings")
+            self.show_alert_popup("There is a problem with cfg file please reset settings and restart Quickshot")
 
     # get options from ui and write them to file (ui -> file)
     def ui_to_variable(self):
@@ -225,20 +330,20 @@ class Qshot_settings(QMainWindow):
             self.new_cfg["ss_options"]["after_number"] = self.after_number_line.text()
             self.new_cfg["ss_options"]["date_formatting"] = self.date_formatting_line.text()
            
-            if(self.local_date_naming_checkbox.isChecked):
-                self.new_cfg["ss_options"]["use_system_local_date_naming"] = "1"
+            if(self.local_date_naming_checkbox.isChecked()):
+                self.new_cfg["ss_options"]["use_system_local_date_naming"] = 1
             else:
-                self.new_cfg["ss_options"]["use_system_local_date_naming"] = ""
+                self.new_cfg["ss_options"]["use_system_local_date_naming"] = 0
 
             # ss options
             self.new_cfg["ss_options"]["ss_extension"] = self.extension_combobox.currentText()
 
             self.new_cfg["ss_options"]["png_compression_level"] = int(self.png_compression_combobox.currentText())
 
-            if(self.multi_screen_checkbox.isChecked):
-                self.new_cfg["ss_options"]["multi_screen"] = "1"
+            if(self.multi_screen_checkbox.isChecked()):
+                self.new_cfg["ss_options"]["multi_screen"] = 1
             else:
-                self.new_cfg["ss_options"]["multi_screen"] = ""
+                self.new_cfg["ss_options"]["multi_screen"] = 0
 
             return True
         except Exception as e:
@@ -281,6 +386,7 @@ class Qshot_settings(QMainWindow):
 
     def block1(self):
         self.block1_group_box = QGroupBox("Colors and opacity")
+        self.block1_group_box.setToolTip(self.tooltips["colors_and_opacity"])
 
         b1 = QPushButton()
         b1.setText("Background color")
@@ -316,12 +422,6 @@ class Qshot_settings(QMainWindow):
         self.opacity_label.setText("10.0")
 
 
-        self.preview_checkbox = QCheckBox()
-        self.preview_checkbox.setText("prew")
-        self.preview_checkbox.setObjectName("opacity_prew_checkbox")
-        self.preview_checkbox.clicked.connect(self.on_click)
-
-
         hbox1 = QHBoxLayout()
         hbox2 = QHBoxLayout()
         hbox3 = QHBoxLayout()
@@ -340,7 +440,6 @@ class Qshot_settings(QMainWindow):
         hbox3.addWidget(l1)
         hbox3.addWidget(self.opacity_label)
         hbox3.addWidget(self.opacity_slider)
-        hbox3.addWidget(self.preview_checkbox)
 
         vbox.addLayout(hbox1)
         vbox.addLayout(hbox2)
@@ -350,7 +449,7 @@ class Qshot_settings(QMainWindow):
 
     def block2(self):
         self.block2_group_box = QGroupBox("Hotkeys")
-        self.block2_group_box.setToolTip(self.hotkey_tooltip)
+        self.block2_group_box.setToolTip(self.tooltips["hotkeys"])
 
         l1 = QLabel()
         l1.setText("Screenshot key")
@@ -387,53 +486,69 @@ class Qshot_settings(QMainWindow):
     def block3(self):
         self.block3_group_box = QGroupBox("Path and naming")
 
+        # Save Path
         l1 = QLabel()
         l1.setText("Save Path")
-        
+        l1.setToolTip(self.tooltips["save_path"])
+
         self.save_path_line = QLineEdit()
-        self.save_path_line.setText("")
- 
+        self.save_path_line.setToolTip(self.tooltips["save_path"])
+
+        # Root file
         l2 = QLabel()
         l2.setText("Root file")
+        l2.setToolTip(self.tooltips["root_file"])
 
         self.root_file_line = QLineEdit()
-        self.root_file_line.setText("")
-  
+        self.root_file_line.setToolTip(self.tooltips["root_file"])
+
+        # Text before ss name
         l3 = QLabel()
         l3.setText("Text before ss name")
+        l3.setToolTip(self.tooltips["text_before_ss-text_after_ss"])
 
         self.before_ss_name_line = QLineEdit()
-        self.before_ss_name_line.setText("")
+        self.before_ss_name_line.setToolTip(self.tooltips["text_before_ss-text_after_ss"])
 
+        # Text after ss name
         l4 = QLabel()
         l4.setText("Text after ss name")
+        l4.setToolTip(self.tooltips["text_before_ss-text_after_ss"])
 
         self.after_ss_name_line = QLineEdit()
-        self.after_ss_name_line.setText("")
+        self.after_ss_name_line.setToolTip(self.tooltips["text_before_ss-text_after_ss"])
 
+        # Text before ss number
         l5 = QLabel()
         l5.setText("Text before ss number")
+        l5.setToolTip(self.tooltips["text_before_ss_number-text_after_ss_number"])
 
         self.before_number_line = QLineEdit()
-        self.before_number_line.setText("")
+        self.before_number_line.setToolTip(self.tooltips["text_before_ss_number-text_after_ss_number"])
 
+        # Text after ss number
         l6 = QLabel()
         l6.setText("Text after ss number")
+        l6.setToolTip(self.tooltips["text_before_ss_number-text_after_ss_number"])
 
         self.after_number_line = QLineEdit()
-        self.after_number_line.setText("")
+        self.after_number_line.setToolTip(self.tooltips["text_before_ss_number-text_after_ss_number"])
 
-
+        # Date formatting
         l7 = QLabel()
         l7.setText("Date formatting")
+        l7.setToolTip(self.tooltips["date_formatting"])
 
         self.date_formatting_line = QLineEdit()
-        self.date_formatting_line.setText("")
-    
+        self.date_formatting_line.setToolTip(self.tooltips["date_formatting"])
 
+        # Use local date naming
         l8 = QLabel()
         l8.setText("Use local date naming")
+        l8.setToolTip(self.tooltips["Use_local_date_naming"])
+
         self.local_date_naming_checkbox = QCheckBox()
+        self.local_date_naming_checkbox.setToolTip(self.tooltips["Use_local_date_naming"])
 
 
         hbox1 = QHBoxLayout()
@@ -493,25 +608,32 @@ class Qshot_settings(QMainWindow):
 
     def block4(self):
         self.block4_group_box = QGroupBox("Screeshot options")
-
+        
+        # extension
         l1 = QLabel()
         l1.setText("Extension")
+        l1.setToolTip(self.tooltips["extension"])
 
         self.extension_combobox = QComboBox()
         self.extension_combobox.addItems(self.ss_extensions_list)
+        self.extension_combobox.setToolTip(self.tooltips["extension"])
 
+        # png compression level
         l2 = QLabel()
         l2.setText("png compression level")
+        l2.setToolTip(self.tooltips["png_compression_level"])
 
         self.png_compression_combobox = QComboBox()
         self.png_compression_combobox.addItems(self.png_compression_level_list)
+        self.png_compression_combobox.setToolTip(self.tooltips["png_compression_level"])
 
-
+        # Multi screen full ss
         l4 = QLabel()
         l4.setText("Multi screen full ss")
+        l4.setToolTip(self.tooltips["multi_screen_full_ss"])
 
         self.multi_screen_checkbox = QCheckBox()
-        
+        self.multi_screen_checkbox.setToolTip(self.tooltips["multi_screen_full_ss"])
 
         hbox1 = QHBoxLayout()
         hbox2 = QHBoxLayout()
@@ -544,7 +666,7 @@ class Qshot_settings(QMainWindow):
 
         l1 = QLabel()
         l1.setText("Hover over sections for tooltips")
-        l1.setFont(QFont('Arial', 9, weight=QtGui.QFont.Bold))
+        l1.setFont(QFont('Arial', 8, weight=QtGui.QFont.Bold))
 
         b1 = QPushButton()
         b1.setText("Save changes")
@@ -598,6 +720,7 @@ class Qshot_settings(QMainWindow):
         """shows alert popup with given message"""
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
+        msg.setWindowIcon(QtGui.QIcon(self.icon_path))
         msg.setWindowTitle("Alert")
         msg.setText(alert_str)
         msg.exec()
@@ -606,6 +729,7 @@ class Qshot_settings(QMainWindow):
         """shows success popup with given message"""
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
+        msg.setWindowIcon(QtGui.QIcon(self.icon_path))
         msg.setWindowTitle("Success")
         msg.setText(success_str)
         msg.exec()
@@ -614,6 +738,7 @@ class Qshot_settings(QMainWindow):
         """shows about popup"""
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
+        msg.setWindowIcon(QtGui.QIcon(self.icon_path))
         msg.setWindowTitle("About")
         msg.setTextFormat(Qt.RichText)
         msg.setText(self.about_text)
@@ -646,63 +771,52 @@ class Qshot_settings(QMainWindow):
         self.revert_preview()
 
     def update_opacity(self, value):
-        """updates opacity value on frame and if prew checkbox checked also emits signal to main frame to update opacity"""
+        """updates opacity value on frame and emits signal to main frame to update opacity"""
         self.opacity_label.setText(str(value/100))
+        self.oppcity_emitter.emit(value/100)
 
-        if(self.preview_checkbox.isChecked()):
-            self.oppcity_emitter.emit(value/100)
-
-            # self.setWindowOpacity(value/100)
-            
-    
     def on_click(self):
         """on click function for listeners"""
         sender = self.sender()
 
         # buttons
         if(sender.objectName() == "background_color_button"):
+            """writes background color code to lineedit, shows color on color label, emits color change signal to main frame"""
             color = QColorDialog.getColor()
             if(color.isValid()):
                 self.background_color_line.setText(color.name())
                 self.background_color_prew.setStyleSheet("background-color: {0};".format(color.name()))
-                if(self.preview_checkbox.isChecked()):
-                    self.background_color_emitter.emit(color.name())
+                self.background_color_emitter.emit(color.name())
             
         elif(sender.objectName() == "accent_color_button"):
+            """writes accent color code to lineedit, shows color on color label, emits color change signal to main frame"""
             color = QColorDialog.getColor()
             if(color.isValid()):
                 self.accent_color_line.setText(color.name())
                 self.accent_color_prew.setStyleSheet("background-color: {0};".format(color.name()))
-                if(self.preview_checkbox.isChecked()):
-                    self.accent_color_emitter.emit(color.name())
+                self.accent_color_emitter.emit(color.name())
 
         elif(sender.objectName() == "reset_button"):
+            """writes default values to cfg file, reads cfg file and updates ui, emits signal to main frame for updating, shows success popup (write_cfg_file has error popup so I don't need here)"""
             write_status = self.write_cfg_file(self.default_cfg)
             if(write_status):
-                self.show_success_popup("Settings reset to default, please restart the Qshot for changes to be effective")
                 self.read_cfg_file()
                 self.variable_to_ui()
-                if(self.preview_checkbox.isChecked()):
-                    self.activate_preview()
- 
+                self.update_fame_emitter.emit()
+                self.show_success_popup("Settings reset to default")
+
         elif(sender.objectName() == "save_changes_button" or sender.objectName() == "save" ):
+            """takes settings from ui writes them to cfg file, emits signal to main frame for updating, shows success popup (write_cfg_file has error popup so I don't need here)"""
             variable_status = self.ui_to_variable()
             if(variable_status):
                 write_status = self.write_cfg_file(self.new_cfg)
                 if(write_status):
-                    self.show_success_popup("Settings saved, please restart the Qshot for changes to be effective")
-
-
-        # checkboxes
-        elif(sender.objectName() == "opacity_prew_checkbox"):
-            if(self.preview_checkbox.isChecked()):
-                self.activate_preview()
-            else:
-                self.revert_preview()
+                    self.update_fame_emitter.emit()
+                    self.show_success_popup("Settings saved")
 
         # menu items
         elif(sender.objectName() == "exit"):
-            sys.exit()
+            self.close()
         elif(sender.objectName() == "about"):
             self.show_about_popup()
 
@@ -711,7 +825,7 @@ class Qshot_settings(QMainWindow):
 
 
 def start_settings_with_event_loop(cfg_path, icon_path):
-    """if the main window can not open because of the cfg file, settings needs its own event loop to stay open"""
+    """if the main window can not open because of the cfg file is being broken, settings needs its own event loop to stay open"""
     application = QApplication(sys.argv)    
 
     settings = Qshot_settings(cfg_path, icon_path)
