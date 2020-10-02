43874782ad76dbdf19b8cea9c8a15689d5a8c4af
can kurt <can.kurt.aa@gmail.com>
2020-10-02 04:00:47 +0300

    tray icon logic is now insde the Quickshot class, because this makes more sense

--- Quickshot/main.py
+++ Quickshot/main.py
@@ -1,46 +1,14 @@
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication
 from Quickshot import Qshot
 import sys
 
-def build_application(cfg_path, tray_icon_path):
+def build_application(cfg_path, icon_path):
     app = QApplication(sys.argv)
-
-
-    # instantiate frame
-    frame = Qshot(cfg_path=cfg_path, icon_path=tray_icon_path)
-
-    # create tray menu
-    menu = QMenu()
-    take_ss_action = menu.addAction("Take ss")
-    take_ss_action.triggered.connect(frame.take_ss)
-
-    hide_show_action = menu.addAction("Hide/Show")
-    hide_show_action.triggered.connect(frame.hide_show)
-
-    settings = menu.addAction("Settings")
-    settings.triggered.connect(frame.show_settings)
-
-    exit_action = menu.addAction("Exit")
-    exit_action.triggered.connect(app.quit)
-
-    # create tray
-    tray_icon = QSystemTrayIcon(QIcon(tray_icon_path), parent = app)
-    tray_icon.setToolTip("Quickshot")
-    tray_icon.setContextMenu(menu)
-    tray_icon.show()
-
-    
+    frame = Qshot(cfg_path=cfg_path, icon_path=icon_path)
     sys.exit(app.exec())
-
-
 
 if __name__ == '__main__':
     build_application("Quickshot/cfg/Qshot.cfg", "Quickshot/icons/Qs.ico")
 
     # pyinstaller
-    # build_application("cfg/Qshot.cfg", "Quickshot/icons/Qs.ico")
-
-
-
-
+    # build_application("cfg/Qshot.cfg", "icons/Qs.ico")
